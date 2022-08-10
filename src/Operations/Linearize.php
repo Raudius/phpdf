0059196919fb8ff0d85654f6f9ccc710924dd377
Raul <r.ferreira.fuentes@gmail.com>
2022-08-10 18:03:51 +0200

    Remove constructor from Linearize

--- src/Operations/Linearize.php
+++ src/Operations/Linearize.php
@@ -11,18 +11,6 @@ use function raudius\phpdf\qpdf;
  * @url https://qpdf.readthedocs.io/en/stable/linearization.html
  */
 class Linearize extends Operation {
-    /** @var string|null */
-    private $password;
-
-    /**
-     * Optionally a password can be provided to unlock the file if the file is password protected.
-     *
-     * @param string|null $password
-     */
-    public function __construct(string $password = null) {
-        $this->password = $password;
-    }
-
     /**
      * @param Phpdf $pdf
      * @throws PhpdfException
